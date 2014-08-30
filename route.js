09b9e850eb4e841b451afda9204287a6bcebc5fd
izgzhen <izgzhen@gmail.com>
2014-08-30 23:00:36 +0800

    API

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var level = require('level');
 var db = level('./data');
 
-/* GET home page. */
+/* GET pages */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Index' });
 });
@@ -12,42 +12,61 @@ router.get('/create', function(req, res) {
   res.render('create', { title: 'Create List' });
 });
 
-router.post('/create', function(req, res){
-	var words = req.body.data.slice(0, -1).split(';');
-	var options = words.map(function(word){
-		return {
-			type : 'put',
-			key : word,
-			value : {
-				status : 'new'
-			},
-			valueEncoding : 'json'
-		}
+/* API */
+
+router.route('/words/:word')
+	.delete(function(req, res){ // Delete a word from DB
+		var word = req.params.word;
+		db.del(word, function(err){
+			if (err) msg = 'error';
+			else msg = 'ok';
+			res.json({ msg : msg });
+		});
+	})
+	.put(function(req, res){	// Update status of words
+		var word = req.params.word;
+		var status = req.body.status;
+		db.put(word, { status : status }, { valueEncoding : 'json' }, function(err){
+			if (err) msg = 'error';
+			else msg = 'ok';
+			res.json({ msg : msg });
+		});
 	})
 
-	db.batch(options, function (err) {
-	  if (err) return console.log('Ooops!', err)
-	  console.log('Great success dear leader!')
+router.route('/words')
+	.post(function(req, res){
+		var words = req.body.data.slice(0, -1).split(';');
+		var options = words.map(function(word){
+			return {
+				type : 'put',
+				key : word,
+				value : {
+					status : 'new'
+				},
+				valueEncoding : 'json'
+			};
+		});
+		db.batch(options, function (err) {
+			if (err) throw err;
+			console.log('Great success dear leader!');
+		});
+		res.json({msg: "Complete, Sir!"});
 	})
-
-	res.end("Complete, Sir!")
-})
-
-router.get('/getWords', function(req, res){
-	var words = '';
-	db.createKeyStream()
-	  .on('data', function (data) {
-		words += data + ','
-	  })
-	  .on('error', function (err) {
-	    console.log('Oh my!', err)
-	  })
-	  .on('close', function () {
-	    console.log('Stream closed')
-	  })
-	  .on('end', function () {
-  		res.send(words);
-	  });
-})
+	.get('/words', function(req, res){
+		var words = [];
+		db.createReadStream()
+		  	.on('data', function (data) {
+		    	words.push({ word : data.key, attribute: JSON.parse(data.value) });
+	  		})
+	  		.on('error', function (err) {
+		    	console.log('Oh my!', err)
+	  		})
+		  	.on('close', function () {
+			    console.log('Stream closed')
+		  	})
+		  	.on('end', function () {
+	  			res.json(words);
+		  	});
+	})
 
 module.exports = router;
