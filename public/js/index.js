7d9de79df22ca0f9f524bf6ffc14689a01d43265
izgzhen <izgzhen@gmail.com>
2014-08-31 12:04:13 +0800

    Optimize Index

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,37 @@
 (function(){
 	var app = angular.module('index', []);
-  app.controller('MainController', function($scope, $http){
+  	app.controller('MainController', function($scope, $http){
 		this.count = 0;
 		this.currentWord = 'Placeholder';
+		this.spelling = '';
 		$scope.words = [];
+		this.testInfo = '';
+
 		this.loadList = function(){
 		  $http.get('/words').success(function (data) {
-			 $scope.words.push(data.words[0].word);
+				$scope.words = data.words.map(function(obj){
+			 		return obj.word;
+				})
 			});
 		}
 
 		this.goNext = function(){
 			this.count = (this.count + 1) % $scope.words.length;
 			this.currentWord = $scope.words[this.count];
+		}
+
+		this.sound = function(){
+			var msg = new SpeechSynthesisUtterance(this.currentWord);
+			window.speechSynthesis.speak(msg);
+		}
+
+		this.iForgot = function(){
+			this.testInfo = "I forget!";
+		}
+
+		this.checkSpelling = function(){
+			if(this.spelling == this.currentWord)
+				this.testInfo = "Passed!";
 		}
 	});
 })();
